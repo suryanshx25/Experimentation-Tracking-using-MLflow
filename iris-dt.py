bbccc87f49afcae020ac80031ebe6eda66d31e2f
Suryansh Singh <suryanshx25@gmail.com>
2024-09-01 15:30:41 +0530

    add another exp

--- iris-dt.py
+++ iris-dt.py
@@ -6,10 +6,11 @@ from sklearn.datasets import load_iris
 
 from sklearn.model_selection import train_test_split
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.metrics import accuracy_score, confusion_matrix
 
+mlflow.set_experiment('iris.dt')
 
 # Load the Iris dataset
 
@@ -25,14 +26,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_s
 #Train a Random Forest classifier
 
 max_depth = 15
-n_estimators = 10
 
 with mlflow.start_run():
-    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_test)
+    dt = DecisionTreeClassifier(max_depth=max_depth)
+    dt.fit(X_train, y_train)
+    y_pred = dt.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     mlflow.log_metric('accuracy',accuracy)
     mlflow.log_param('max_depth',max_depth)
-    mlflow.log_param('n_estimators',n_estimators)
     print('accuracy' ,accuracy)
